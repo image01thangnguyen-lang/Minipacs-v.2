d19ad90b2d0273915756e1efcf2383075e2785e3
image01thangnguyen-lang <image01.thangnguyen@gmail.com>
2026-06-30 22:30:34 +0700

    feat: add Nginx configuration for OHIF and implement default commands module for viewer extensions

--- ohif-viewer/extensions/default/src/commandsModule.ts
+++ ohif-viewer/extensions/default/src/commandsModule.ts
@@ -350,8 +350,9 @@ const commandsModule = ({
      * Changes the viewport grid layout in terms of the MxN layout.
      */
     setViewportGridLayout: ({ numRows, numCols }) => {
-      const { protocol } = hangingProtocolService.getActiveProtocol();
-      const onLayoutChange = protocol.callbacks?.onLayoutChange;
+      const activeProtocol = hangingProtocolService.getActiveProtocol();
+      const protocol = activeProtocol?.protocol;
+      const onLayoutChange = protocol?.callbacks?.onLayoutChange;
       if (commandsManager.run(onLayoutChange, { numRows, numCols }) === false) {
         console.log('setViewportGridLayout running', onLayoutChange, numRows, numCols);
         // Don't apply the layout if the run command returns false
